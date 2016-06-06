fffb5e365121bd63c2891612b10e739b5739cc8f
Brayden Girard <brayden@daydreamdev.com>
2016-06-05 20:01:07 -0400

    Added warning

--- js/variables.js
+++ js/variables.js
@@ -2,27 +2,27 @@
  * Javascript template variables
  */
 
-var	mobileMenuTitle = "Menu",					//The title of the mobile menu
-	
-	hero100PercentHeight = false,				//If true, the hero section (home) will be set with a minimum height of 100% window height. If false, hero height will be the height of its content.
-	
-	//GOOGLE MAP VARIABLES
-	map_canvas_id = "map_canvas",				//The HTML "id" of the map canvas
-	map_color = "#939499",						//Google map color
-	map_initial_zoom = 15,						//The initial zoom when Google map loads
-	map_initial_latitude = null,			//Google map initial Latitude. If "null", the latitude of the first marked will be used
-	map_initial_longitude = null,		//Google map initial Longitude. If "null", the longitude of the first marked will be used
-	use_default_map_style = false,				//If true, default map style will be used
-	
-	
-	//CONTACT FORM VARIABLES
-	contact_form_success_msg = "Form successfully submitted! :)",
-	contact_form_error_msg = "Error sending message :(",
-	
-	
-	//COUNTDOWN VARIABLES
-	c_days = "DAYS",							//Countdown "Days" label
-	c_hours = "HOURS",							//Countdown "Hours" label
-	c_minutes = "MIN.",							//Countdown "Minutes" label
-	c_seconds = "SEC.",							//Countdown "Seconds" label
-	countdownEndMsg = "Event Started!";			//Message to display when the countdown reaches the end
+var mobileMenuTitle = "Menu", //The title of the mobile menu
+
+  hero100PercentHeight = false, //If true, the hero section (home) will be set with a minimum height of 100% window height. If false, hero height will be the height of its content.
+
+  //GOOGLE MAP VARIABLES
+  map_canvas_id = "map_canvas", //The HTML "id" of the map canvas
+  map_color = "#939499", //Google map color
+  map_initial_zoom = 15, //The initial zoom when Google map loads
+  map_initial_latitude = null, //Google map initial Latitude. If "null", the latitude of the first marked will be used
+  map_initial_longitude = null, //Google map initial Longitude. If "null", the longitude of the first marked will be used
+  use_default_map_style = false, //If true, default map style will be used
+
+
+  //CONTACT FORM VARIABLES
+  contact_form_success_msg = "Form successfully submitted! :)",
+  contact_form_error_msg = "Error sending message :( - This form does not work in Safari, please try a different browser.",
+
+
+  //COUNTDOWN VARIABLES
+  c_days = "DAYS", //Countdown "Days" label
+  c_hours = "HOURS", //Countdown "Hours" label
+  c_minutes = "MIN.", //Countdown "Minutes" label
+  c_seconds = "SEC.", //Countdown "Seconds" label
+  countdownEndMsg = "Event Started!"; //Message to display when the countdown reaches the end
